616c0fe5aace9d89008d5719739a2b1988055ee2
Yoann Abran <y.abran@codeur.online>
2020-10-05 14:46:23 +0200

    test reactnative

---
+++ API/TMDBApi.js
@@ -0,0 +1,12 @@
+const API_TOKEN = "03fd617cbcb5b09e8a6d4d55b69c50d8";
+
+export function getFilmsFromApiWithSearchedText (text,page){
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key='+ API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+  return fetch(url)
+  .then((response) => response.json())
+  .catch((error) => console.error(error))
+}
+
+export function getImageFromApi(name){
+  return 'https://image.tmdb.org/t/p/w300' + name
+}
---
+++ Components/FilmItem.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import {StyleSheet, View, Text, Image} from 'react-native'
+import {getImageFromApi} from '../API/TMDBApi'
+
+class FilmItem extends React.Component {
+  render(){
+    const film = this.props.film
+    return(
+      <View style={styles.main_container}>
+        <Image style = {styles.image}
+                source ={{uri: getImageFromApi(film.poster_path)}}/>
+        <View style={styles.content}>
+      <View style={styles.header}>
+        <Text style={styles.title_text}> {film.title}</Text>
+        <Text style={styles.vote}>{film.vote_average}</Text>
+        </View>
+        <View style={styles.decription}>
+          <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
+        </View>
+        <View style={styles.date}>
+          <Text style={styles.date_text}>{film.release_date}</Text>
+        </View>
+        </View>
+        </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  main_container:{
+    height :190,
+    flexDirection : 'row',
+  },
+  content :{
+    flex : 1,
+    margin: 5,
+  },
+  image :{
+    width :120,
+    height :180,
+    margin :5,
+  },
+  header : {
+    flex:3,
+    flexDirection : 'row'
+  },
+  description :{
+    flex :7,
+  },
+  date : {
+    flex :1,
+  },
+  title_text : {
+    fontWeight: 'bold',
+   fontSize: 20,
+   flex: 1,
+   flexWrap: 'wrap',
+   paddingRight: 5
+ },
+  vote : {
+    fontWeight: 'bold',
+    fontSize: 26,
+    color: '#666666'
+  },
+  description_text: {
+    fontStyle: 'italic',
+    color: '#666666'
+  },
+  date_text: {
+  textAlign: 'right',
+  margin : 5,
+  fontSize: 14
+}
+})
+
+export default FilmItem
